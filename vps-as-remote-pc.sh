c92915d7d37cd31e6a0bdd4945aa719163d55bcb
Manuel de la Fuente <mail@manuelfte.com>
2018-08-12 10:48:39 -0500

    This should solve the issue with the resolv.conf file being ineditable in some systems.

--- vps-as-remote-pc.sh
+++ vps-as-remote-pc.sh
@@ -25,7 +25,9 @@ vncserver
 # dhclient=$( sudo find /etc -name dhclient.conf )
 # echo "supersede domain-name-servers 208.67.222.222,208.67.220.220; # OpenDNS nameservers" >> $dhclient
 # sudo systemctl restart networking
+wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/resolv.conf
 sudo mv /etc/resolv.conf /etc/resolv.conf.bak
-sudo touch /etc/resolv.conf
-sudo echo -e "nameserver 208.67.222.222\nnameserver 208.67.220.220" >> /etc/resolv.conf
+sudo mv resolv.conf /etc
+# sudo touch /etc/resolv.conf
+# sudo echo -e "nameserver 208.67.222.222\nnameserver 208.67.220.220" >> /etc/resolv.conf
 
