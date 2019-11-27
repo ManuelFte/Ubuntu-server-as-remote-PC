7e2ea73e69811614b28092f4ee195798471e154b
Manuel de la Fuente <mail@manuelfte.com>
2019-11-27 12:55:39 -0600

    .

--- README.md
+++ README.md
@@ -2,20 +2,15 @@
 
 ## Description
 
-
-Turns a VPS into a basic remote PC appropriate for downloading and streaming media at high speed. Essentially an automated version of [this tutorial](https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-16-04), with multiple changes:
+Turns a VPS into a basic remote PC. Essentially an automated version of [this tutorial](https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-ubuntu-16-04), with multiple changes:
 
 * LXDE instead of Xfce
-* Google Chrome ~~(because who uses Firefox nowadays)~~
+* Chromium ~~(because who uses Firefox nowadays)~~
 * OpenDNS
-* youtube-dl
-* jDownloader (downloads script, doesn't install)
-* Nginx
+* DWService (downloads script, doesn't install)
 
-Tested on Ubuntu 16.04 on DigitalOcean and Google Cloud.
+Tested on Ubuntu 16.04 on OVH.
 
 ## Usage
 
 * On the VPS: `cd ~ && wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/vps-as-remote-pc.sh && bash vps-as-remote-pc.sh`
-* While it installs, do this on your local machine: `ssh -L 5901:127.0.0.1:5901 -N -f -l <VPS user> <VPS IP>`
-* After it has finished, connect your VNC client (I use [Remmina](https://www.remmina.org)) to `localhost:5901` with your VPS user and the password chosen during installation.
--- vps-as-remote-pc.sh
+++ vps-as-remote-pc.sh
@@ -1,28 +1,9 @@
 #!/bin/bash
-# Adds PPAs for youtube-dl and Google Chrome
-sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 # Mandatory stuff
 sudo apt update
 sudo apt upgrade -y
-# Installs LXDE, TightVNC Server, youtube-dl, Chrome and Nginx
-sudo apt install lubuntu-desktop tightvncserver youtube-dl google-chrome-stable nginx -y
-# Starts VNC server to set initial configuration
-vncserver
-# Kills it in order to override settings
-vncserver -kill :1
-# Backup because why not
-mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-# Downloads new configuration file
-cd ~/.vnc
-wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/xstartup
-# Executable permissions
-chmod +x xstartup
-# Returns to user folder
-cd ~
-# Launches again
-vncserver
+# Installs LXDE, Chromium, and DWService
+sudo apt install lubuntu-desktop chromium-browser -y
 # OpenDNS
 # dhclient=$( sudo find /etc -name dhclient.conf )
 # echo "supersede domain-name-servers 208.67.222.222,208.67.220.220; # OpenDNS nameservers" >> $dhclient
@@ -32,19 +13,3 @@ sudo mv /etc/resolv.conf /etc/resolv.conf.bak
 sudo mv resolv.conf /etc
 # sudo touch /etc/resolv.conf
 # sudo echo -e "nameserver 208.67.222.222\nnameserver 208.67.220.220" >> /etc/resolv.conf
-#Downloads jDownloader
-#wget https://aotwiki.com/u/jdownloader.sh
-#Sets permissions for the Nginx directory
-#sudo mkdir -p /var/www/stream.aotwiki.com/html
-# $sudo chown -R $USER:www-data /var/www/stream.aotwiki.com/html
-# #Downloads the pages for streams
-# #cd /var/www/stream.aotwiki.com/html
-# wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/streams.zip
-# unzip streams.zip
-# rm streams.zip
-# cd ~
-# #Configures server
-# wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/stream.aotwiki.com
-# sudo mv stream.aotwiki.com /etc/nginx/sites-available
-# sudo ln -s /etc/nginx/sites-available/stream.aotwiki.com /etc/nginx/sites-enabled
-# sudo systemctl reload nginx
