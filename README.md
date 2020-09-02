ee0d5bb0cd8626abbdcc92a559cf062e6bbd4c0f
Manuel de la Fuente <git@manuelfte.com>
2020-09-02 06:53:45 -0500

    Removed DWservice as apparently it has stopped working. Added instructions for TightVNC instead.

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 Turns a VPS into a basic remote PC. Installed software:
 
 * LXQt
+* TightVNC Server
 * Chromium
 * OpenDNS
-* DWService (downloads script, doesn't install)
 
 ## Usage
 
@@ -16,3 +16,11 @@ git clone https://ManuelFte@bitbucket.org/ManuelFte/vps-as-remote-pc.git
 cd vps-as-remote-pc
 sh vps-as-remote-pc.sh
 ```
+
+To connect, type this in the client machine:
+
+`ssh -L 59000:127.0.0.1:5901 user@IP -N -v -v`
+
+Then use a VNC program like TigerVNC to connect to `127.0.0.1:59000`.
+
+<!--https://serverfault.com/questions/489192/ssh-tunnel-refusing-connections-with-channel-2-open-failed-->
--- vps-as-remote-pc.sh
+++ vps-as-remote-pc.sh
@@ -5,24 +5,20 @@ sudo ln -s /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 # Update system
 sudo apt update
 sudo apt upgrade -y
-# Installs LXQt, Chromium, and DWService
-sudo apt install lxqt sddm chromium-browser -y
-wget https://www.dwservice.net/download/dwagent_x86.sh
+# Installs LXQt, TightVNC Server, Chromium
+sudo apt install lxqt openbox sddm tightvncserver chromium-browser -y
 # Starts VNC server to set initial configuration
-# vncserver
+vncserver
 # Kills it in order to override settings
-# vncserver -kill :1
+vncserver -kill :1
 # Backup because why not
-# mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 # Downloads new configuration file
-# cd ~/.vnc
-# wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/xstartup
+mv xstartup ~/.vnc
 # Executable permissions
-# chmod +x xstartup
-# Returns to user folder
-# cd ~
+chmod +x ~/.vnc/xstartup
 # Launches again
-# vncserver
+vncserver
 # OpenDNS
 # dhclient=$( sudo find /etc -name dhclient.conf )
 # echo "supersede domain-name-servers 208.67.222.222,208.67.220.220; # OpenDNS nameservers" >> $dhclient
