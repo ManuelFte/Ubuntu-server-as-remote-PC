29620f400757b62b2b00ae01e082aa7e8730c287
Manuel de la Fuente <git@manuelfte.com>
2020-09-02 02:27:30 -0500

    I don't remember what I did.

--- vps-as-remote-pc.sh
+++ vps-as-remote-pc.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
+# Update server time
+sudo mv /etc/localtime /etc/localtime.old
+sudo ln -s /usr/share/zoneinfo/America/Mexico_City /etc/localtime
 # Mandatory stuff
 sudo apt update
 sudo apt upgrade -y
-# Installs LXDE, Chromium, and DWService
-sudo apt install lubuntu-desktop chromium-browser -y
+# Installs LXDE, TightVNC Server, Chromium, and DWService
+sudo apt install lubuntu-desktop tightvncserver chromium-browser -y
+wget https://www.dwservice.net/download/dwagent_x86.sh
+# Starts VNC server to set initial configuration
+vncserver
+# Kills it in order to override settings
+vncserver -kill :1
+# Backup because why not
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+# Downloads new configuration file
+cd ~/.vnc
+wget https://bitbucket.org/ManuelFte/VPS-as-remote-PC/raw/master/xstartup
+# Executable permissions
+chmod +x xstartup
+# Returns to user folder
+cd ~
+# Launches again
+vncserver
 # OpenDNS
 # dhclient=$( sudo find /etc -name dhclient.conf )
 # echo "supersede domain-name-servers 208.67.222.222,208.67.220.220; # OpenDNS nameservers" >> $dhclient
